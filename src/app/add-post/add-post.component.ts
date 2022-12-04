d4608a0adf02551a9b137225195b3b226ccc07c4
sherin200 <sherinsebastian21>
2022-12-04 21:45:47 +0530

    add

---
+++ src/app/add-post/add-post.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-post',
+  templateUrl: './add-post.component.html',
+  styleUrls: ['./add-post.component.css']
+})
+export class AddPostComponent {
+  userId=""
+  id=""
+  title=""
+  body=""
+
+  readValue=()=>
+  {
+    let data:any={
+      "userId":this.userId,
+      "id":this.id,
+      "title":this.title,
+      "body":this.body
+    }
+    console.log(data)
+  }
+
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AddPostComponent } from './add-post/add-post.component';
+import { RouterModule, Routes } from '@angular/router';
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AddPostComponent
+    
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute) 
+   ],
   providers: [],
   bootstrap: [AppComponent]
 })
